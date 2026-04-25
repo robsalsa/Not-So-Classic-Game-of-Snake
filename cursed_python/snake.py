6316eb70eec0841b82f5829ba608118bcdae9b04
Robert <robertosalazarvasquezalexis@gmail.com>
2026-04-25 14:22:15 -0700

    Set Up: Not Done

---
+++ cursed_python/snake.py
@@ -0,0 +1,5 @@
+import pyautogui
+
+print("\n WARNING: This program will steal your mouse for a bit")
+print("It is recomended that you do not press anything other than the designated buttons")
+print("... or dont i dont really care tbh\n")
