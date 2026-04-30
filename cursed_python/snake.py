170771ea0fee7e42baa2a7518de2303266da8eb7
Robert <robertosalazarvasquezalexis@gmail.com>
2026-04-29 17:36:10 -0700

    math fixed and it now moves but no pick ups

--- cursed_python/snake.py
+++ cursed_python/snake.py
@@ -1,46 +1,32 @@
 import pyautogui
 import keyboard
-
-# from PIL import ImageGrab
-import random
-import time 
-#the grid is 20-long x 9-high = 180 spots to move to.
+import time
 
 width=20
 hight=9
 
-app_distance_x = 85     #my math is off FIX IT IDIOT
+app_distance_x = 95
 app_distance_y = 105
-
 
 app_starting_line_x = 50
 app_starting_line_y = 30
-
 
 current_x= app_starting_line_x
 current_y= app_starting_line_y
 
 grid = []
 
-
-snake_head=(0,0)
+snake_body=[(0,0)]
 direction="Stop"
 running=True
 
-print("\n WARNING: This program will steal your mouse for a bit")
-print("It is recomended that you do not press anything other than the designated buttons")
-print("... or dont i dont really care tbh\n")
 
 
-
-
-
-# Set Up the game: Creating a Grid to play and easy finding of items
 def set_grid():
-    cols = 21 #actual is 19 but for full play is 20 and buffer is 21
-    rows = 10 #actual is 8 but full play is 9 and buffer is 10  
+    cols = 19
+    rows = 9
     global grid
-    
+    grid=[]
     for innie in range (rows):
         row_center=[]
         for outtie in range (cols):
@@ -48,78 +34,63 @@ def set_grid():
             center_y =  app_starting_line_y + (innie * app_distance_y)
             row_center.append((center_x, center_y))
         grid.append(row_center)
-    return grid
-    
-    
+
 def snake_location():
-    global snake_head
-    snake_head = (0,0)
     x,y=grid[0][0]
     pyautogui.moveTo(x,y)
-    
-    
-    
-def go_down():
-    global direction
-    if direction != "up":
-        direction = "down"
-def go_up():
-    global direction     
-    if direction != "down":
-        direction = "up"
-def go_right():
-    global direction         
-    if direction !="left":
-        direction = "right"
-def go_left():
-    global direction  
-    if direction != "right":
-        direction = "left"
-        
-        
-def move():
-    global snake_head, direction
-    row,col=snake_head
-    
-    if direction == "up":
-        row -= 1
-    elif direction == "down":
-        row+=1
-    elif direction =="right":
-        col+=1
-    elif direction == "left":
-        col-=1
-        
-    if 0<=row < len(grid) and 0<=col <len(grid[0]):
-        snake_head=(row,col)
-        
-        x,y = grid[row][col]
-        pyautogui.moveTo(x,y)
-        
+
 def button_comp(event):
     global direction, running
-    if event.name == 'w':
+    if event.name == 'w' and direction != "down":
         direction = "up"
-    elif event.name == 's':
+    elif event.name == 's' and direction != "up":
         direction = "down"
-    elif event.name == 'a':
+    elif event.name == 'a' and direction != "right":
         direction = "left"
-    elif event.name == 'd':
+    elif event.name == 'd' and direction != "left":
         direction = "right"
     elif event.name == 'p':
         running = False
-        print("GAME KILLED!")
+
+keyboard.on_press(button_comp)
+
+def get_next(pos):
+    row,col = pos
+    if direction == "up":
+        return (row-1, col)
+    elif direction == "down":
+        return (row+1, col)
+    elif direction == "left":
+        return (row, col-1)
+    elif direction == "right":
+        return (row, col+1)
+    return pos
+
+def perform_step():
+    global snake_body
+
+    head = snake_body[0]
+    next_pos = get_next(head)
+
+    if not (0 <= next_pos[0] < len(grid) and 0 <= next_pos[1] < len(grid[0])):
+        return
+
+    x,y=grid[next_pos[0]][next_pos[1]]
+    pyautogui.moveTo(x,y)
+    snake_body[0]=next_pos
     
-    
-keyboard.on_press(button_comp)        
+    # # NORMAL MOVE (no apple)
+    # if next_pos not in initial_apples:
+    #     x,y = grid[next_pos[0]][next_pos[1]]
+    #     pyautogui.moveTo(x,y)
+    #     snake_body[0] = next_pos
+    #     return
+
+
 set_grid()
 snake_location()
-print("controls are wasd")
+pyautogui.moveTo(*grid[0][0])
 
 while running:
-    move()
-    time.sleep(0.3)
-
-
-
- 
+    perform_step()
+    # time.sleep(0.1)
--- testing_mouse_mover.py
+++ testing_mouse_mover.py
@@ -1,3 +1,22 @@
 import pyautogui
 
-pyautogui.moveTo(0,0)
+# pyautogui.moveTo(46,30) #dead-on
+#pyautogui.moveTo(46, 135) #dead-on row-2, col-1
+pyautogui.moveTo(140, 30)
+
+# for innie in range (9):
+#     y = 30+(105*innie)
+ #   pyautogui.moveTo(46,y,duration=0.8)
+
+for outtie in range (19):
+    x=46+(95*outtie)
+    pyautogui.moveTo(x,30)
+
+
+
+
+
+
+
+# x,y = pyautogui.position()
+# print(x,y)
